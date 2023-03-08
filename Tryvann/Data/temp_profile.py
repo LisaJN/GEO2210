68d0f71ccb3683f0bef5200349872c1a9173ff6d
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2023-03-08 17:16:19 +0100

    Update temp_profile.py

--- Tryvann/Data/temp_profile.py
+++ Tryvann/Data/temp_profile.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 
 # Get data
 snow_temp = pd.read_csv("snow_temp.csv", header=0, names=['depth','temp'])
+snow_temp_2 = pd.read_csv("snow_temp_2.csv", header=0, names=['depth','temp'])
 
-# Plot of snow temperature
-plt.plot(snow_temp["temp"],snow_temp["depth"])
+# Plot temperature profile
+plt.plot(snow_temp['temp'],snow_temp['depth'],'c')
+plt.plot(snow_temp_2['temp'],snow_temp_2['depth'],'--')
 plt.xlabel("Temperature")
 plt.ylabel("Depth")
 plt.grid()
+plt.legend(['Original','Comparison'])
 plt.title("Temperature as a fuction of depth")
 plt.show()
